c91f95f81c2adda2785693928977fc6554796edf
Jorge <jgomez@scl.edu.gt>
2025-08-19 09:11:06 -0600

    galeria productos

--- modulos/item/itemModulo.js
+++ modulos/item/itemModulo.js
@@ -1,12 +1,23 @@
-export function item(titulo,urlImg){
+export function item(titulo,urlImg,description,price){
 
     let item = document.createElement('div');
     item.className = "item";
-    item.textContent = titulo;
+
+    let h3 = document.createElement('h3');
+    h3.textContent = titulo;
+    item.appendChild(h3);
     
     let img = document.createElement('img');
     img.src = urlImg;
     item.appendChild(img);
+
+    let p = document.createElement('p');
+    p.textContent = description;
+    item.appendChild(p);
+
+    let span = document.createElement('span');
+    span.textContent = "$" + price;
+    item.appendChild(span);
 
     item.addEventListener('click', ()=>{
             item.classList.add("verde");
